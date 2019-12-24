c4cbb0adebec5394ff00071249d728ab2324d791
github username <github email address>
2019-12-23 20:26:25 -0700

    added function for searching for news

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,6 +18,14 @@ class Nav extends Component {
         this.setState({searchTerm: event.target.value})
     }
 
+    getNews = (searchTerm) => {
+        fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=fd5ace6ad0b54f9c9dae1cc5004a9fb5`)
+            .then(response => response.json())
+            .then(news => {
+                this.setState({ news })
+            })
+    }
+
 
 
 
@@ -31,6 +39,9 @@ class Nav extends Component {
                 <Route
                     exact path='/'
                     render={(props) => <Home {...props} 
+                    searchTerm={this.state.searchTerm}
+                    getNews={this.getNews(this.state.searchTerm)}
+                    onNewsSearch={this.onNewsSearch}
                     news={this.state.news}/>}
                 />
             </Router>
